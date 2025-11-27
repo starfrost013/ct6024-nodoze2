4a65b0d3d9b57ddb24be184b12c9cc902e9f90c8
starfrost013 <mario64crashed@gmail.com>
2025-11-27 22:55:09 +0000

    i ruined everything

--- Assets/Code/AssetManager/AssetManager.cs
+++ Assets/Code/AssetManager/AssetManager.cs
@@ -1,39 +1,36 @@
 ﻿using UnityEngine;
 using System;
 
-namespace Domino
-{   
-    // This is a static class that allows the loading and tracking of assets
-    internal static class AssetManager
+// This is a static class that allows the loading and tracking of assets
+internal static class AssetManager
+{
+    // todo: type determination stuff 
+    // possibly have a list of assets here. but then unity could internally unload stuff and we'd have no way to tell 
+    private static UInt32 numAssetsLoaded;
+
+    internal static T LoadAsset<T>(string path) where T : UnityEngine.Object
     {
-        // todo: type determination stuff 
-        // possibly have a list of assets here. but then unity could internally unload stuff and we'd have no way to tell 
-        private static UInt32 numAssetsLoaded;
+        // https://stackoverflow.com/questions/552629/c-sharp-print-the-class-name-from-within-a-static-function
+        // Reflection is type safe, but 50 times slower!!!!
+        Debug.Log("Game Asset Loader: Loading asset of type " + typeof(T).Name + "");
 
-        internal static T LoadAsset<T>(string path) where T : UnityEngine.Object
+        T newAsset = Resources.Load<T>(path);
+
+        if (!newAsset)
         {
-            // https://stackoverflow.com/questions/552629/c-sharp-print-the-class-name-from-within-a-static-function
-            // Reflection is type safe, but 50 times slower!!!!
-            Debug.Log("Game Asset Loader: Loading asset of type " + typeof(T).Name + "");
-
-            T newAsset = Resources.Load<T>(path);
-
-            if (!newAsset)
-            {
-                Debug.LogError("***** FAILED to load asset ******");
-                return null; 
-            }
-
-            numAssetsLoaded++;
-
-            return newAsset; 
+            Debug.LogError("***** FAILED to load asset ******");
+            return null;
         }
 
-        internal static void UnloadAsset<T>(T asset) where T : UnityEngine.Object
-        {
-            Resources.UnloadAsset(asset);
+        numAssetsLoaded++;
 
-            numAssetsLoaded--;
-        }
+        return newAsset;
+    }
+
+    internal static void UnloadAsset<T>(T asset) where T : UnityEngine.Object
+    {
+        Resources.UnloadAsset(asset);
+
+        numAssetsLoaded--;
     }
 }
