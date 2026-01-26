f1056fedd41bc9f0e4be12bfe8d764441aaf5f20
starfrost013 <mario64crashed@gmail.com>
2026-01-26 18:50:52 +0000

    nvm

--- Assets/Code/GameObjects/BasePhysicsObject.cs
+++ Assets/Code/GameObjects/BasePhysicsObject.cs
@@ -20,7 +20,6 @@ internal class BasePhysicsObject : MonoBehaviour
         {
             Debug.LogWarning("It's probably a good idea to add the rigidbody component to the BasePhysicsObject " + name + ". Adding automatically...");
             thisRigidbody = gameObject.AddComponent<Rigidbody>();
-            thisRigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
         }
 
 
