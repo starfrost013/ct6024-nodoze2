0d07735525521c83377ed6e371c1c72d3627850d
starfrost013 <mario64crashed@gmail.com>
2026-01-30 20:35:09 +0000

    TMP + Anti-annoyance

---
+++ Assets/Code/GameModes/GameModeRaceFinished.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+// The gamemode for when the race is finished
+internal class GameModeRaceFinished  : GameMode
+{
+    internal string raceConfigFile;
+
+    internal override void OnEnter()
+    {
+    }
+
+    internal override void OnFrame()
+    {
+    }
+
+    internal override void OnFixedUpdate()
+    {
+
+    }
+
+    internal override void OnLeave()
+    {
+
+    }
+}
