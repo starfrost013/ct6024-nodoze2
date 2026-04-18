791cb75674b77a69097042e28cd55dab6d0f61ce
starfrost013 <mario64crashed@gmail.com>
2026-04-18 23:12:50 +0100

    properly import carsports. carbasic had a weird box collider which was stupid and was removed

--- Assets/Code/UI/CarSelectUIController.cs
+++ Assets/Code/UI/CarSelectUIController.cs
@@ -2,5 +2,25 @@
 using UnityEngine;
 
 public class CarSelectUIController : MonoBehaviour
-{ 
+{
+    public void Start()
+    {
+        
+    }
+
+    public void PrevClicked()
+    {
+
+    }
+
+    public void NextClicked()
+    {
+
+    }
+
+    public void DoneClicked()
+    {
+        // we know what th egame state is here, so don't change it 
+        // HACK! 
+    }
 }
